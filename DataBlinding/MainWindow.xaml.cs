3f1eaae5dda59d809db3cf7ceb33aacb34f980f6
Vlados <hanovvladik@mail.ru>
2025-11-16 18:37:10 +0300

    ssfs

--- DataBlinding/MainWindow.xaml.cs
+++ DataBlinding/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using DataBlinding.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,45 @@ namespace DataBlinding
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Person> Peiole { get; set; }
+        Person person = new Person
+        {
+            Name = "Vlad",
+            Age = 18
+        };
+        public List<Person> People = new List<Person>
+        {
+            new Person{Name = "Denis", Age = 30},
+            new Person{Name = "Anton", Age = 18},
+            new Person{Name = "Vladimir", Age = 10},
+            new Person{Name = "Stas", Age = 50},
+        };
         public MainWindow()
         {
             InitializeComponent();
+            ListBoxNames.ItemsSource = People;
+            //ListBoxNames.ItemsSource = new List<string>()
+            //{
+            //    "Denis",
+            //    "Anton",
+            //    "Vladimir",
+            //    "Stas"
+            //};
+            //this.DataContext = person;
+        }
+
+        private void helloBTN_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{person.Name} is {person.Age} years old.");
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = ListBoxNames.SelectedItems;
+            foreach (var item in selectedItems)
+            {
+                var person = (Person)item;
+                MessageBox.Show(person.Name);
+            }
         }
     }
 }
